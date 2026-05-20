974d9c1e2f5d2140ac553b8c895d492141b4dfee
SarahRaven <220771817+SarahRaven@users.noreply.github.com>
2026-05-20 19:50:09 -0400

    Deathrattle modified to track critical state from death

--- Content.Shared/Mobs/Systems/MobStateSystem.StateMachine.cs
+++ Content.Shared/Mobs/Systems/MobStateSystem.StateMachine.cs
@@ -109,6 +109,7 @@ public partial class MobStateSystem
 
         OnExitState(target, component, oldState);
         component.CurrentState = newState;
+        component.PreviousState = oldState;
         OnEnterState(target, component, newState);
 
         var ev = new MobStateChangedEvent(target, component, oldState, newState, origin);
